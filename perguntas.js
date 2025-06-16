c2165d17590df458180991fa31c727c5b8044adc
josianeruiz1976 <Josiane.ruiz@escola.pr.gov.br>
2025-06-16 20:46:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Física',
+    'O que é velocidade?',
+    'Velocidade é a medida da variação da posição de um corpo em um certo tempo.'
+)
+
+criaCartao(
+    'Física',
+    'O que é energia?',
+    'Energia é a capacidade de realizar trabalho ou provocar mudanças.'
+)
+
+criaCartao(
+    'Física',
+    'O que é corrente elétrica?',
+    'Corrente elétrica é o movimento ordenado de elétrons em um condutor.'
+)
+
+criaCartao(
+    'Física',
+    'O que é resistência elétrica?',
+    'Resistência elétrica é a oposição que um material oferece à passagem da corrente.'
+)
